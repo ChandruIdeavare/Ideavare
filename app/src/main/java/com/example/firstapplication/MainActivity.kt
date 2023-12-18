93f2d84a875f36e1dec1aff093ddcf80390af20c
ChandruIdeavare <153673300+ChandruIdeavare@users.noreply.github.com>
2023-12-18 23:14:58 +0530

    new updates

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.firstapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    private val LOCATION_REQUEST_CODE = 1000
+
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf("Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION"),
+            LOCATION_REQUEST_CODE
+        )
     }
-}
+    }
